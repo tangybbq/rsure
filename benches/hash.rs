ece56cc833a9aead278d9abe4a6074298c91acdd
David Brown <davidb@davidb.org>
2016-06-07 20:40:00 -0600

    Increase the size of the hash benchmark
    Some investigation shows that having the benchmark fit in the L3 cache
    results in slightly faster results.  Make this more realistic by
    creating 16MB of file data, and adding a comment that the value may
    need to be adjusted for a particular CPU's cache.

--- benches/hash.rs
+++ benches/hash.rs
@@ -12,14 +12,20 @@ use std::fs::File;
 use std::io::Write;
 use test::Bencher;
 
-// To compute hashing speed, use 1 over the benchmark time in seconds.  For
-// example, if the benchmark runs in 1,863,225 ns/iter, that would be about
-// 536 MiB/sec hash performance.
+// To compute hashing speed, use 1 over the benchmark time in seconds, and
+// then multiply the result by the number of iterations in the 'for i'
+// loop.  For example, if the benchmark runs in 29,924,583 ns/iter, and the
+// count is 16, that would be about 534 MiB/sec hash performance.
+//
+// The loop count should be large enough to overflow the CPU's largest
+// cache, with the value 16 (16MiB) overflowing the 8MiB cache on the Core
+// i7-950 I wrote this on.
 #[bench]
 fn tree_mb_bench(b: &mut Bencher) {
     let tmp = TempDir::new("rsure-bench").unwrap();
-    {
-        let mut fd = File::create(tmp.path().join("large")).unwrap();
+    for i in 0 .. 16 {
+        let name = format!("large-{}", i);
+        let mut fd = File::create(tmp.path().join(&name)).unwrap();
         let buf = vec![0; 1024];
         for _ in 0 .. 1024 {
             fd.write_all(&buf).unwrap();
