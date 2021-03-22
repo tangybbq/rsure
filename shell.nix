514bcdfa46ceaf9f063ebe65469e3c7b00b116f1
David Brown <davidb@davidb.org>
2021-03-21 23:18:26 -0600

    nix: Add cssc build
    Weave has additional tests it can run if SCCS is available in the path.
    The tests are based around the GNU version of this strange little
    revision control system.

--- shell.nix
+++ shell.nix
@@ -1,5 +1,27 @@
 # Shell configuration to build rsure.
 { pkgs ? import <unstable> {} }:
+let
+  lib = pkgs.lib;
+  stdenv = pkgs.stdenv;
+
+  # SCCS isn't particularly useful, but the file used by weave is
+  # derived from what SCCS uses.  If this program is in the path, then
+  # weave has additional tests that it can run.
+  cssc = stdenv.mkDerivation rec {
+    name = "cssc-1.4.1";
+
+    src = pkgs.fetchurl {
+      url = "mirror://gnu/cssc/CSSC-1.4.1.tar.gz";
+      sha256 = "1vsisqq573xjr2qpn19iwmpqgl3mq03m790akpa4rvj60b4d1gni";
+    };
+
+    meta = with lib; {
+      homepage = "https://www.gnu.org/software/cssc/";
+      description = "GNU replacement for SCCS";
+      license = licenses.gpl3;
+    };
+  };
+in
 pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.openssl.dev
@@ -9,5 +31,7 @@ pkgs.mkShell {
     pkgs.cargo
     pkgs.clippy
     pkgs.rustfmt
+
+    cssc
   ];
 }
