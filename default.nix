d6f85f419400ed2c08c4188df09ef7af1f4b5286
David Brown <davidb@davidb.org>
2022-02-16 11:38:32 -0700

    nix: Update to 0.9.4

--- default.nix
+++ default.nix
@@ -3,23 +3,23 @@
 with import <nixpkgs> {};
 rustPlatform.buildRustPackage rec {
   pname = "rsure";
-  version = "0.9.3";
+  version = "0.9.4";
 
   src = fetchFromGitHub {
     owner = "tangybbq";
     repo = pname;
-    rev = "v0.9.3";
-    sha256 = "0crmds1qqmnx1pkfib2gj9l51g7iw2vy6p3z2jffgcc5s26fv5mb";
+    rev = "v0.9.4";
+    sha256 = "sha256:0bx0l2q64ma057l2wwvsnbgl8jr6szanfwr5311lqqzvp4r4kaqy";
   };
 
-  cargoSha256 = "0mnljanhf530zlf5x8l93rm2pzssjfgwkm741wih10h4d4admdmr";
+  cargoSha256 = "sha256:1bym7z2b3sw9g2hvixagir4bqh0389v9f2r66x2nf871683vc34y";
 
   nativeBuildInputs = [
     pkgs.pkgconfig
   ];
   buildInputs = [ pkgs.openssl.dev pkgs.sqlite.dev ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "A utility for ensuring file integrity";
     homepage = "https://github.com/tangybbq/rsure";
     license = with licenses; [ mit ];
